22547c280d1b987183268b2c1baf727634508276
Neel Sharma <nsdj.sharma@gmail.com>
2023-11-29 21:41:43 +1100

    reduce code

--- common.py
+++ common.py
@@ -15,13 +15,6 @@ def pixels_to_fraction(mat, x, y):
     return x / width, y / height
 
 
-def point_average(*points):
-    return (
-        sum(point.x for point in points) / len(points),
-        sum(point.y for point in points) / len(points),
-    )
-
-
 def dist_between_squared(x1, y1, x2, y2):
     return (y2 - y1) ** 2 + (x2 - x1) ** 2
 
@@ -43,4 +36,4 @@ def is_pinch(landmarks):
 def get_pinch_pointer(landmarks):
     index = landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
     thumb = landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
-    return point_average(index, thumb)
+    return (index.x + thumb.x) / 2, (index.y + thumb.y) / 2
